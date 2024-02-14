784bf1a3e7552ea154443f72ad01c107227df62d
Liam <davisliam123@gmail.com>
2024-02-13 22:00:50 -0500

    wrote setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="Poker Math",
+    url="https://github.com/liamjdavis/Poker-Math.git",
+    description="tool for simulating poker",
+    author="Liam Davis",
+    license="MIT",
+    python_requires="3.11>=",
+    packages=["pokermath"],
+)
