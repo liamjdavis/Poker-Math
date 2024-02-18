aa0d396a3137ac29193c22147a76e161aee46ecc
liamjdavis <66973153+liamjdavis@users.noreply.github.com>
2024-02-18 12:22:12 -0800

    start hand classifier

--- pokermath/game.py
+++ pokermath/game.py
@@ -12,6 +12,7 @@ class Game:
         self.deck = Deck()
         self.community_cards = []
         self.pot = 0
+        self.hand_rankings = self.set_handRankigs()
     
     # game loop
     def play_round(self):
@@ -19,16 +20,19 @@ class Game:
         self.deal_cards()
         self.collect_blinds()
 
+        # reset community cards
+        self.community_cards = []
+
         # set loop flop, turn and river
         for i in range(5):
             self.add_communityCard
 
             # player logic
             for player in self.players:
-                # all players bet
+                # default: all players bet blinds amount
                 player.bet(self.blinds)
         
-        self.getWinner()
+        winner = self.getWinner(self.players, self.community_cards)
     
     # shuffle then deal cards
     def deal_cards(self):
@@ -49,5 +53,55 @@ class Game:
         self.community_cards.append(self.deck.deal_card)
     
     # determine winner
-    def getWinner():
+    def getWinner(players, community_cards):
+        hands = []
+        winner = None
+
+        # iterate through the players hands
+        for player in players:
+            hand = player.get_hand() + community_cards
+
+            # classify hand
+            hand_class = classify_hand(hand)
+            hands.append(hand)
+
         return
+    
+    # set hand rankings
+    def set_handRankings():
+        rankings = {
+            "High Card": 0,
+            "One Pair": 1,
+            "Two Pair": 2,
+            "Three of a Kind": 3,
+            "Straight": 4,
+            "Flush": 5,
+            "Full House": 6,
+            "Four of a Kind": 7,
+            "Straight Flush": 8,
+            "Royal Flush": 9
+        }
+
+        return rankings
+
+    # hand classifier
+    def classify_hand(hand):
+        # give new variable for sorted hand
+        sorted_hand = sorted(hand for card in hand)
+
+        # count occurrences of each suit
+        suit_counts = {}
+
+        for card in hand:
+            if card.suit in suit_counts:
+                suit_counts[card.suit] += 1
+            else:
+                suit_counts[card.suit] = 1
+        
+        num_suits = len(suit_counts)
+
+        # check for royal flush
+        royal_flush = ["10", "Jack", "Queen", "King", "Ace"]
+
+        if sorted_hand == royal_flush:
+            return "Royal Flush"
